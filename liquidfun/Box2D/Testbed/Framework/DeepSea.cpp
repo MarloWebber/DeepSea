7de3d47896c78407d852241338c3dd6bc73031d2
Marlo <marlo@livestocklabs.com>
2020-09-08 16:51:52 +1000

    work through compilation errors

--- liquidfun/Box2D/Testbed/Framework/DeepSea.cpp
+++ liquidfun/Box2D/Testbed/Framework/DeepSea.cpp
@@ -42,7 +42,7 @@ b2Vec2 rotatePoint(float cx,float cy,float angle, b2Vec2 p) {
 	return b2Vec2(p.x,p.y);
 };
 
-jointUserData_t::jointUserData_t() {
+JointUserData::JointUserData( b2World * m_world, b2ParticleSystem * m_particleSystem) {
 	torque = 0.0f; 	
 	speed = 0.0f; 	
 	speedLimit = 0.0f;
@@ -54,13 +54,28 @@ jointUserData_t::jointUserData_t() {
 	normalAngle = 0.0f;
 	lowerAngle = 0.0f;
 
-	joint = nullptr; 	// the joint that this user data struct gets pinned to
-
-	init = false;
+	// init = false;
 	isUsed = false;
+
+	// the following code prepares the box2d objects.
+	jointDef.bodyA = attachedTo->p_body;
+	jointDef.bodyB = attaches->p_body;
+	jointDef.localAnchorA =  attachedTo->tipCenter;
+	jointDef.localAnchorB =  attachedTo->tipCenter;
+	jointDef.enableLimit = true;
+	jointDef.lowerAngle = lowerAngle;
+	jointDef.upperAngle = upperAngle;
+	jointDef.enableMotor = true;
+    jointDef.maxMotorTorque = torque;
+    jointDef.motorSpeed = speed;
+
+    jointDef.userData = this;
+    // joint = jointDef; 	// the joint that this user data struct gets pinned to
+
+    init = true;
 }
 
-boneUserData_t::boneUserData_t() {
+BoneUserData::BoneUserData(b2World * m_world, b2ParticleSystem * m_particleSystem) {
 
 	// initialize everything to default, sane values
 	length = 0.1f;
@@ -69,9 +84,7 @@ boneUserData_t::boneUserData_t() {
 	density = 1.0f;
 
 	tipCenter = b2Vec2(0.0f,0.1f); 	// these are used so the skeleton master can remember his place as he traverses the heirarchy of souls.
-	rootCenter = b2Vec2(0.0f,0.0f); 
-
-	joint = new jointUserData_t; 	// the joint that attaches it into its socket			
+	rootCenter = b2Vec2(0.0f,0.0f); 			
 
 	isRoot = false;
 	isMouth = false;
@@ -82,13 +95,13 @@ boneUserData_t::boneUserData_t() {
 	energy = 0.0f; 					// the nutritive energy stored in the tissue of this limb; used by predators and scavengers
 
 	// the following code is used to generate box2d structures and shapes from the bone parameters.
-	float angle = p_bone->joint->normalAngle;
+	float angle = joint->normalAngle;
 
-	b2Vec2 tipCenter = b2Vec2(p_bone->attachedTo->tipCenter.x, p_bone->attachedTo->tipCenter.y + p_bone->length);
-	b2Vec2 rootVertexA = b2Vec2(p_bone->attachedTo->tipCenter.x + (p_bone->rootThickness/2), p_bone->attachedTo->tipCenter.y);
-	b2Vec2 rootVertexB = b2Vec2(p_bone->attachedTo->tipCenter.x - (p_bone->rootThickness/2), p_bone->attachedTo->tipCenter.y);
-	b2Vec2 tipVertexA = b2Vec2(tipCenter.x + (p_bone->tipThickness/2), tipCenter.y);
-	b2Vec2 tipVertexB = b2Vec2(tipCenter.x - (p_bone->tipThickness/2), tipCenter.y);
+	b2Vec2 tipCenter = b2Vec2(attachedTo->tipCenter.x, attachedTo->tipCenter.y + length);
+	b2Vec2 rootVertexA = b2Vec2(attachedTo->tipCenter.x + (rootThickness/2), attachedTo->tipCenter.y);
+	b2Vec2 rootVertexB = b2Vec2(attachedTo->tipCenter.x - (rootThickness/2), attachedTo->tipCenter.y);
+	b2Vec2 tipVertexA = b2Vec2(tipCenter.x + (tipThickness/2), tipCenter.y);
+	b2Vec2 tipVertexB = b2Vec2(tipCenter.x - (tipThickness/2), tipCenter.y);
 
 	int count = 4;
 	b2Vec2 vertices[count];
@@ -98,59 +111,40 @@ boneUserData_t::boneUserData_t() {
 	vertices[3].Set(rootVertexA.x, rootVertexA.y);
 
 	// figure out the center point.
-	b2Vec2 boneCenter = b2Vec2(p_bone->attachedTo->tipCenter.x, p_bone->attachedTo->tipCenter.y + (2*p_bone->length));
-
-	b2BodyDef bd1;
+	b2Vec2 boneCenter = b2Vec2(attachedTo->tipCenter.x, attachedTo->tipCenter.y + (2*length));
 
 	// attach user data to the body
-	bd1.userData = p_bone;
+	bodyDef.userData = this;
 
-	bd1.type = b2_dynamicBody;
-	b2Body* body1 = m_world->CreateBody(&bd1);
-	b2PolygonShape shape1;
-	shape1.SetAsBox(p_bone->rootThickness, p_bone->length, boneCenter, angle);	
+	bodyDef.type = b2_dynamicBody;
+	p_body = m_world->CreateBody(&bodyDef);
+	
+	shape.SetAsBox(rootThickness, length, boneCenter, angle);	
 
 	// reference the physics object from the user data.
-	p_bone->body = body1;
-	p_bone->shape = &shape1;
-	p_bone->tipCenter = tipCenter;
-	p_bone->rootCenter = p_bone->attachedTo->tipCenter;
+	tipCenter = tipCenter;
+	rootCenter = attachedTo->tipCenter;
 
-	// create the joint that pins it to the root bone.
-	if (!p_bone->isRoot) {
-			b2RevoluteJointDef jointDef2;
-			jointDef2.bodyA = p_bone->attachedTo->body;
-			jointDef2.bodyB = p_bone->body;
-			jointDef2.localAnchorA =  p_bone->attachedTo->tipCenter;
-			jointDef2.localAnchorB =  p_bone->attachedTo->tipCenter;
-			jointDef2.enableLimit = true;
-			jointDef2.lowerAngle = p_bone->joint->lowerAngle;
-			jointDef2.upperAngle = p_bone->joint->upperAngle;
-			jointDef2.enableMotor = true;
-            jointDef2.maxMotorTorque = 1.0f;
-            jointDef2.motorSpeed = 0.0f;
+	if (!isRoot) {
+		joint = new JointUserData( m_world, m_particleSystem); 	// the joint that attaches it into its socket 
+	}	
 
-            jointDef2.userData = p_bone->joint;
-
-            p_bone->joint->init = true;
-	}
-
-	init = false;
+	init = true;
 	isUsed=  false;
 };
 
-void nonRecursiveBoneIncorporator(boneUserData_t * p_bone, b2World * m_world, b2ParticleSystem * m_particleSystem) {
-	body1->CreateFixture(&shape1, p_bone->density);	// this endows the shape with mass and is what adds it to the physical world.
-	m_particleSystem->DestroyParticlesInShape(shape1,body1->GetTransform());
+void nonRecursiveBoneIncorporator(BoneUserData * p_bone, b2World * m_world, b2ParticleSystem * m_particleSystem) {
+	p_bone->p_body->CreateFixture(&(p_bone->shape), p_bone->density);	// this endows the shape with mass and is what adds it to the physical world.
+	// m_particleSystem->DestroyParticlesInShape( &(p_bone->shape) ,p_bone->body->GetTransform());
 	if (!p_bone->isRoot) {
             p_bone->joint->isUsed = true;
-			m_world->CreateJoint(&jointDef2);
+			p_bone->joint->p_joint = (b2RevoluteJoint*)m_world->CreateJoint( &(p_bone->joint->jointDef) );
 	}
 	p_bone->isUsed = true;
 }
 
-void nonRecursiveSensorUpdater (boneUserData_t * p_bone) {
-	p_bone->position = p_bone->body->GetPosition();
+void nonRecursiveSensorUpdater (BoneUserData * p_bone) {
+	p_bone->position = p_bone->p_body->GetPosition();
 	p_bone->sensation = 0.0f;
 
 	for  (int i = 0; i < N_FOODPARTICLES; i++) {
@@ -182,157 +176,157 @@ void addFoodParticle ( b2Vec2 position, b2World * m_world, b2ParticleSystem * m_
 	body9->CreateFixture(&shape9, 4.0f);
 	m_particleSystem->DestroyParticlesInShape(shape9,body9->GetTransform()); // snip out the particles that are already in that spot so it doesn't explode
 
-	food[currentNumberOfFood].body = body9;
-	food[currentNumberOfFood].shape = &shape9;
+	food[currentNumberOfFood].p_body = body9;
+	food[currentNumberOfFood].shape = shape9;
 
 	food[currentNumberOfFood].init = true;
 
 	currentNumberOfFood++;
 }
 
-BonyFish::BonyFish()
+BonyFish::BonyFish(b2World * m_world, b2ParticleSystem * m_particleSystem)
 {
 	hunger = 0.0f; // the animal spends energy to move and must replenish it by eating
 	position = b2Vec2(0.0f, 0.0f); // the starting position of the fish in the game world
-	init = false; // true after the particle has been initialized. In most cases, uninitalized particles will be ignored.
+
+	for (int i = 0; i < N_FINGERS; ++i) {
+		bones[i] = new BoneUserData( m_world, m_particleSystem);
+	}
+	init = true; // true after the particle has been initialized. In most cases, uninitalized particles will be ignored.
 	isUsed = false;
 
-	for (int i = 0; i < N_FINGERS; ++i)
-		{
-			bones[i] = new boneUserData_t;
-		}
 }
 
 // this describes the original 3 boned jellyfish.
-void makeAJellyfish (BonyFish * p_fish, b2World * m_world, b2ParticleSystem * m_particleSystem) {
+// void makeAJellyfish (BonyFish * p_fish, b2World * m_world, b2ParticleSystem * m_particleSystem) {
 
-p_fish->bones[0]->joint = {
-	1.0f,
-	0.0f,
-	2.0f,
-	false,
-	false,
-	0.0f,
-	0.0f * pi,
-	0.0f,
-	nullptr,
-	false,
-	true
-};
+// p_fish->bones[0]->joint = {
+// 	1.0f,
+// 	0.0f,
+// 	2.0f,
+// 	false,
+// 	false,
+// 	0.0f,
+// 	0.0f * pi,
+// 	0.0f,
+// 	nullptr,
+// 	false,
+// 	true
+// };
 
-p_fish->bones[1]->joint = {
-	1.0f,
-	0.0f,
-	2.0f,
-	false,
-	false,
-	0.05f,
-	-0.15f * pi,
-	0.25f,
-	nullptr,
-	false,
-	true
-};
+// p_fish->bones[1]->joint = {
+// 	1.0f,
+// 	0.0f,
+// 	2.0f,
+// 	false,
+// 	false,
+// 	0.05f,
+// 	-0.15f * pi,
+// 	0.25f,
+// 	nullptr,
+// 	false,
+// 	true
+// };
 
-p_fish->bones[2]->joint = {
-	1.0f,
-	0.0f,
-	2.0f,
-	false,
-	false,
-	0.05f,
-	0.15f * pi,
-	0.25f,
-	nullptr,
-	false,
-	true
-};
+// p_fish->bones[2]->joint = {
+// 	1.0f,
+// 	0.0f,
+// 	2.0f,
+// 	false,
+// 	false,
+// 	0.05f,
+// 	0.15f * pi,
+// 	0.25f,
+// 	nullptr,
+// 	false,
+// 	true
+// };
 
-p_fish->bones[0] = {
-	0.15f,
-	0.01f,
-	0.01f,
-	1.5f,
-	b2Vec2(0,0),
-	b2Vec2(0,0),
-	&joint3,
-	nullptr,
-	true,
-	true,
-	true,
-	1.0f,
-	false,
-	0.0f,
-	nullptr,
-	nullptr,
-	b2Vec2(0,0),
-	false,
-	true
-};
+// p_fish->bones[0] = {
+// 	0.15f,
+// 	0.01f,
+// 	0.01f,
+// 	1.5f,
+// 	b2Vec2(0,0),
+// 	b2Vec2(0,0),
+// 	&joint3,
+// 	nullptr,
+// 	true,
+// 	true,
+// 	true,
+// 	1.0f,
+// 	false,
+// 	0.0f,
+// 	nullptr,
+// 	nullptr,
+// 	b2Vec2(0,0),
+// 	false,
+// 	true
+// };
 
-p_fish->bones[1] = {
-	0.1f,
-	0.01f,
-	0.01f,
-	1.5f,
-	b2Vec2(0,0),
-	b2Vec2(0,0),
-	&joint1,
-	&bone0,
-	false,
-	false,
-	false,
-	0.0f,
-	false,
-	0.0f,
-	nullptr,
-	nullptr,
-	b2Vec2(0,0),
-	false,
-	true
-};
+// p_fish->bones[1] = {
+// 	0.1f,
+// 	0.01f,
+// 	0.01f,
+// 	1.5f,
+// 	b2Vec2(0,0),
+// 	b2Vec2(0,0),
+// 	&joint1,
+// 	&bone0,
+// 	false,
+// 	false,
+// 	false,
+// 	0.0f,
+// 	false,
+// 	0.0f,
+// 	nullptr,
+// 	nullptr,
+// 	b2Vec2(0,0),
+// 	false,
+// 	true
+// };
 
-p_fish->bones[2]= {
-	0.1f,
-	0.01f,
-	0.01f,
-	1.5f,
-	b2Vec2(0,0),
-	b2Vec2(0,0),
-	&joint2,
-	&bone0,
-	false,
-	false,
-	false,
-	0.0f,
-	false,
-	0.0f,
-	nullptr,
-	nullptr,
-	b2Vec2(0,0),
-	false,
-	true
-};
+// p_fish->bones[2]= {
+// 	0.1f,
+// 	0.01f,
+// 	0.01f,
+// 	1.5f,
+// 	b2Vec2(0,0),
+// 	b2Vec2(0,0),
+// 	&joint2,
+// 	&bone0,
+// 	false,
+// 	false,
+// 	false,
+// 	0.0f,
+// 	false,
+// 	0.0f,
+// 	nullptr,
+// 	nullptr,
+// 	b2Vec2(0,0),
+// 	false,
+// 	true
+// };
 
-p_fish = {
-	{bone0,bone1,bone2,nullptr,nullptr,nullptr,nullptr,nullptr},
-	0.0f,
-	b2Vec2(0.0f,2.5f),
-	false,
-	true
-}; 
+// p_fish = {
+// 	{bone0,bone1,bone2,nullptr,nullptr,nullptr,nullptr,nullptr},
+// 	0.0f,
+// 	b2Vec2(0.0f,2.5f),
+// 	false,
+// 	true
+// }; 
 
-for (int i = 0; i < N_FINGERS; ++i)
-{
-	if (p_fish->bones[i].isUsed) {
-		nonRecursiveBoneIncorporator(p_fish->bones[i], m_world, m_particleSystem) ;
-	}
-}
+// for (int i = 0; i < N_FINGERS; ++i)
+// {
+// 	if (p_fish->bones[i].isUsed) {
+// 		nonRecursiveBoneIncorporator(p_fish->bones[i], m_world, m_particleSystem) ;
+// 	}
+// }
 
-p_fish->init = true;
-p_fish->isUsed = true;
+// p_fish->init = true;
+// p_fish->isUsed = true;
 
-}
+// }
 
 void deepSeaSetup (b2World * m_world, b2ParticleSystem * m_particleSystem) {
 
@@ -341,7 +335,7 @@ void deepSeaSetup (b2World * m_world, b2ParticleSystem * m_particleSystem) {
 
 	// initialize 10 fish
 
-	makeAJellyfish(&fishes[0], m_world, m_particleSystem);
+	// makeAJellyfish(&fishes[0], m_world, m_particleSystem);
 
 }
 
@@ -351,7 +345,7 @@ void deepSeaLoop () {
 	// get positions of all the food particles
 	for  (int i = 0; i < N_FOODPARTICLES; i++) {
 		if (food[i].init) {
-			food[i].position = food[i].body->GetPosition();
+			food[i].position = food[i].p_body->GetPosition();
 		}
 	}
 
--- liquidfun/Box2D/Testbed/Framework/DeepSea.h
+++ liquidfun/Box2D/Testbed/Framework/DeepSea.h
@@ -8,68 +8,64 @@
 #define N_FISHES 8
 #define N_FOODPARTICLES 8
 
-struct jointUserData_t {
+b2Vec2 rotatePoint(float cx,float cy,float angle, b2Vec2 p);
+
+struct JointUserData; // forward class declaration
+struct BoneUserData;
+struct BonyFish;
+
+struct JointUserData {
 	float torque; 	
 	float speed; 	
 	float speedLimit;
-
 	bool driveCW;	// a signal that tells the motor to turn in one direction. This is much simpler than trying to drive it with a number and having to remember positon etc. With this, you just hit the button, or don't.
 	bool driveCCW;	// a signal that tells the motor to turn in the other direction.
-
 	float upperAngle;
 	float normalAngle;
 	float lowerAngle;
 
-	b2RevoluteJoint * joint; // the joint that this user data struct gets pinned to
+	b2RevoluteJointDef jointDef;
+	b2RevoluteJoint * p_joint; // the joint that this user data struct gets pinned to
 
 	bool init;
 	bool isUsed;
+
+	BoneUserData * attaches;
+	BoneUserData * attachedTo;
+
+	JointUserData(b2World * m_world, b2ParticleSystem * m_particleSystem) ;
 } ;
 
-
-struct boneUserData_t {
-
+struct BoneUserData {
 	float length;
 	float rootThickness;
 	float tipThickness;
 	float density;
-
 	b2Vec2 tipCenter; 				// these are used so the skeleton master can remember his place as he traverses the heirarchy of souls.
 	b2Vec2 rootCenter; 
-
-	jointUserData_t * joint; 		// the joint that attaches it into its socket		
-	boneUserData_t * attachedTo;	
-
+	JointUserData * joint; 		// the joint that attaches it into its socket		
+	BoneUserData * attachedTo;	
 	bool isRoot ;
 	bool isMouth ;
-
 	bool isSensor ;
 	float sensation;
-
 	bool isWeapon ;					// weapons destroy joints to snip off a limb for consumption. optionally, they can produce a physical effect.
-
 	float energy; 					// the nutritive energy stored in the tissue of this limb; used by predators and scavengers
 
-	b2Body * body;
-	b2PolygonShape * shape; 
+	b2BodyDef bodyDef;
+	b2Body * p_body;
+	b2PolygonShape shape;
 
 	b2Vec2 position;
-
 	bool init;		// if the struct has been created properly
 	bool isUsed;	// if the bone is actually used in the game, presumably not all animals will use all 8
 
-	boneUserData_t();
+	BoneUserData(b2World * m_world, b2ParticleSystem * m_particleSystem) ;
 } ;
 
-b2Vec2 rotatePoint(float cx,float cy,float angle, b2Vec2 p);
-
-void fishBrainCreator ();
-
-class BonyFish
+struct BonyFish
 {
-	
-public:
-	boneUserData_t bones[N_FINGERS]; // for now, let's just get fish working with a small, hard-linked, flat level set of bones.
+	BoneUserData bones[N_FINGERS]; // for now, let's just get fish working with a small, hard-linked, flat level set of bones.
 
 	float hunger; // the animal spends energy to move and must replenish it by eating
 	b2Vec2 position; // the starting position of the fish in the game world
@@ -83,8 +79,8 @@ struct foodParticle_t {
 	b2Vec2 position; 			// starting position of the food in the game world
 	float energy; 				// the nutritive value of the food
 
-	b2Body * body;
-	b2CircleShape * shape; 
+	b2Body * p_body;
+	b2CircleShape shape; 
 
 	bool init; 					// true after the particle has been initialized. In most cases, uninitalized particles will be ignored.
 	bool isUsed;
